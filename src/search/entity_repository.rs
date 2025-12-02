1bca8aacd41fa73d7c59dfbae73813bfb2afbd0e
cfsen <103598332+cfsen@users.noreply.github.com>
2025-12-02 12:50:25 +0100

    update: add .desktop files to entity repository

--- src/search/entity_repository.rs
+++ src/search/entity_repository.rs
@@ -1,11 +1,13 @@
 use std::path::PathBuf;
 
-use crate::search::{entity_loader::scan_path_executables, entity_model::{ExecutableEntity, FileEntity, RipgrepEntity}};
+use crate::search::{entity_loader::{scan_desktop_executables, scan_path_executables}, entity_model::{ExecutableEntity, FileEntity, RipgrepEntity}};
 
 pub struct EntityRepository {
+    pub exec_desktop: Vec<ExecutableEntity>,
     pub executables: Vec<ExecutableEntity>,
     pub documents: Vec<RipgrepEntity>,
 
+    pub generic_exec_desktop: Vec<FileEntity>,
     pub generic_executables: Vec<FileEntity>,
     // pub generic_documents: Vec<FileEntity>,
 
@@ -14,19 +16,24 @@ pub struct EntityRepository {
 impl EntityRepository {
     pub fn new(config: RepositoryConfig) -> Self {
         EntityRepository {
+            exec_desktop: Vec::new(),
             executables: Vec::new(),
             documents: Vec::new(),
+            generic_exec_desktop: Vec::new(),
             generic_executables: Vec::new(),
             config,
         }
     }
     pub fn populate(&mut self) -> &mut Self {
+        self.exec_desktop.clear();
         self.executables.clear();
         self.documents.clear();
         self.generic_executables.clear();
 
+        self.exec_desktop = scan_desktop_executables();
         self.executables = scan_path_executables();
         self.generic_executables = EntityRepository::build_generic_executables(&self.executables);
+        self.generic_exec_desktop = EntityRepository::build_generic_executables(&self.exec_desktop);
 
         self
     }
@@ -41,6 +48,10 @@ impl EntityRepository {
     pub fn get_generic_executables(&self) -> &Vec<FileEntity> {
         &self.generic_executables
     }
+    pub fn get_generic_exec_desktop(&self) -> &Vec<FileEntity> {
+        &self.generic_exec_desktop
+    }
+
     pub fn build_generic_executables(entities: &Vec<ExecutableEntity>) -> Vec<FileEntity> {
         entities.iter()
             .cloned()
