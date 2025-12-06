2a9added8a681e7effd2fe354004938b059831f6
cfsen <103598332+cfsen@users.noreply.github.com>
2025-12-06 14:47:48 +0100

    add: hyprland fields newtype macros

---
+++ src/hyprland/macros.rs
@@ -0,0 +1,42 @@
+//
+// newtype macros
+//
+#[macro_export]
+macro_rules! id_type {
+    ($name:ident) => {
+        #[derive(Debug, Serialize, Deserialize, Clone, Hash, Eq, PartialEq)]
+        #[serde(transparent)]
+        pub struct $name(i32);
+
+        impl From<i32> for $name {
+        fn from(value: i32) -> Self { Self(value) }
+        }
+
+        impl TryFrom<&str> for $name {
+        type Error = std::num::ParseIntError;
+        fn try_from(value: &str) -> Result<Self, Self::Error> {
+        value.parse::<i32>().map(Self)
+        }
+        }
+    };
+}
+#[macro_export]
+macro_rules! string_type {
+    ($name:ident) => {
+        #[derive(Debug, Serialize, Deserialize, Clone, Hash, Eq, PartialEq)]
+        #[serde(transparent)]
+        pub struct $name(String);
+
+        impl From<String> for $name {
+        fn from(value: String) -> Self { Self(value) }
+        }
+        impl From<&str> for $name {
+        fn from(value: &str) -> Self { Self(value.to_string()) }
+        }
+        impl std::fmt::Display for $name {
+        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+        }
+        }
+    };
+}
--- src/hyprland/mod.rs
+++ src/hyprland/mod.rs
@@ -2,3 +2,6 @@ pub mod error;
 pub mod events;
 pub mod hyprctl;
 pub mod structs;
+
+#[macro_use]
+mod macros;
