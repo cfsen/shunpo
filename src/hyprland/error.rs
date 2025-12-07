a6fee48536cc2f942e30abea270240fe526c6579
cfsen <103598332+cfsen@users.noreply.github.com>
2025-12-07 13:13:43 +0100

    update: hyprerrror types, add display trait

--- src/hyprland/error.rs
+++ src/hyprland/error.rs
@@ -1,8 +1,37 @@
 pub enum HyprError {
+    EventParseFailed,
+
+    MonitorFailedGetWorkspace,
     MonitorIdExists,
-    WorkspaceIdExists,
     MonitorIdNotFound,
+    MonitorNoneFocused,
+
+    ParseIntError,
+
+    WorkspaceIdExists,
     WorkspaceIdNotFound,
-    Unspecified,
+
     NotImplemented,
+    Unspecified,
+}
+impl std::fmt::Display for HyprError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            HyprError::EventParseFailed => write!(f, "EventParseFailed"),
+            HyprError::MonitorFailedGetWorkspace => write!(f, "MonitorFailedGetWorkspace"),
+            HyprError::MonitorIdExists => write!(f, "MonitorIdExists"),
+            HyprError::MonitorIdNotFound => write!(f, "MonitorIdNotFound"),
+            HyprError::MonitorNoneFocused => write!(f, "MonitorNoneFocused"),
+            HyprError::ParseIntError => write!(f, "ParseIntError"),
+            HyprError::WorkspaceIdExists => write!(f, "WorkspaceIdExists"),
+            HyprError::WorkspaceIdNotFound => write!(f, "WorkspaceIdNotFound"),
+            HyprError::NotImplemented => write!(f, "NotImplemented"),
+            HyprError::Unspecified => write!(f, "Unspecified"),
+        }
+    }
+}
+impl From<std::num::ParseIntError> for HyprError {
+    fn from(_: std::num::ParseIntError) -> Self {
+        HyprError::ParseIntError
+    }
 }
