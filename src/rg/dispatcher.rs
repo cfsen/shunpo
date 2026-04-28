63c35cd44e3f428c36862b2c41c284fd2e6c0127
cfsen <103598332+cfsen@users.noreply.github.com>
2026-04-28 10:28:33 +0200

    update: rg dispatcher load template from config

--- src/rg/dispatcher.rs
+++ src/rg/dispatcher.rs
@@ -1,4 +1,4 @@
-use crate::search::entity_model::{self, CustomDispatcher};
+use crate::{config::config::ShunpoConfig, search::entity_model::{self, CustomDispatcher}};
 
 pub fn create_default() -> CustomDispatcher {
     let mut rg_dispatcher = entity_model::CustomDispatcher {
@@ -14,4 +14,35 @@ pub fn create_default() -> CustomDispatcher {
     };
     rg_dispatcher.validate_template();
     rg_dispatcher
+}
+
+pub fn create_from_config(config: &ShunpoConfig) -> Option<CustomDispatcher> {
+    let mut rg_dispatcher = entity_model::CustomDispatcher {
+        alias: "Ripgrep dispatcher".to_string(),
+        requires: vec![
+            "$term".to_string(),
+            "$editor".to_string(),
+            "$path".to_string(),
+            "$line".to_string(),
+        ],
+        template: config.editor_dispatch.clone(),
+        valid: false,
+    };
+
+    if rg_dispatcher.validate_template() {
+        Some(rg_dispatcher)
+    }
+    else {
+        None
+    }
+}
+
+pub fn from_config_or_default(config: &ShunpoConfig) -> CustomDispatcher {
+    if let Some(dispatcher) = create_from_config(&config) {
+        dispatcher
+    }
+    else {
+        log::warn!("Using default dispatcher for ripgrep");
+        create_default()
+    }
 }
