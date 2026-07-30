114f166b4e74436f6713a864197142021651c18b
cfsen <103598332+cfsen@users.noreply.github.com>
2026-07-30 18:09:18 +0200

    fix: bump gtk4 embedded style priority
    Increase priority for using embedded gtk4 styling.

--- src/ui_gtk4/builder.rs
+++ src/ui_gtk4/builder.rs
@@ -44,7 +44,7 @@ pub fn build_ui(
         gtk4::style_context_add_provider_for_display(
             &display,
             &provider,
-            gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION,
+            gtk4::STYLE_PROVIDER_PRIORITY_USER + 1,
         );
     }
 
